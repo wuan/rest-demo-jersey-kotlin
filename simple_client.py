bb00313f6c535be616ffc1a1b268b987a1459f48
Andreas Würl <andi@tryb.de>
2017-10-31 11:17:11 +0100

    use hyperschema client

--- simple_client.py
+++ simple_client.py
@@ -1,48 +1,29 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
-import json
-import requests
-
-headers={'content-type': 'application/json'}
+from hyperschema import Link
+from requests import Session
 
 base_url = "http://localhost:9090/api"
 
-def dump_links(response):
-    for link in response['_schema']['links']:
-        print("    {}: {} {} {}".format(link['rel'], link['method'], link['href'], link['schema'] if 'schema' in link else ''))
+session = Session()
+#  session.verify = False
+session.headers['content-type'] = 'application/json'
 
-def do_request(method, url):
-    response = requests.get(url)
-    response_json = response.json()
+root = Link(base_url, session=session).follow()
 
-    print('{}: {}'.format(method, url))
-    dump_links(response_json)
+stations = root.follow('stations')
 
-    return response_json
+print('*** create station')
 
-print("*** get base")
-do_request('GET', base_url)
+station = stations.follow('create', {'name': 'test', 'longitude': 11.0, 'latitude': 49.0})
+print("created: {}".format(station))
 
-print()
-print("*** get stations")
-stations = do_request('GET', base_url + '/stations')
-for station in stations['members']:
-  print("   ", station['name'], station['longitude'], station['latitude'], station['id'])
+stations = stations.follow('self')
+print(stations)
 
-print()
-print("*** create station")
-response = requests.post(base_url + "/stations", data=json.dumps({'name': 'test', 'longitude': 11.0, 'latitude': 49.0}), headers=headers).json()
-dump_links(response)
-station_id = response['id']
+station.follow('delete')
 
-print()
-print("*** get stations")
-stations = do_request('GET', base_url + '/stations')
-for station in stations['members']:
-  print("   ", station['name'], station['longitude'], station['latitude'], station['id'])
-
-do_request('GET', base_url + '/stations/{}'.format(station_id))
+stations = stations.follow('self')
+print(stations)
 
 
